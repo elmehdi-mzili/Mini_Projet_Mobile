dc5c3f380b0b97872aac04e391140bcf16dd62c0
elmehdi-mzili <elmehdi.mzili@gmail.com>
2020-07-13 00:48:30 +0100

    hello

---
+++ Navigation/Navigation.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native'
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+import Search from '../Components/Search'
+import FilmDetail from '../Components/FilmDetail'
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+class Navigation extends React.Component{
+  render(){
+    return(
+     // <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Rechercher" component={Search}/>
+          <Stack.Screen name="FilmDetail" options={{ title: 'Film detail' }} component={FilmDetail}/>
+        </Stack.Navigator>
+    //  </NavigationContainer>
+      // <NavigationContainer>
+      //   <Tab.Navigator>
+      //     <Tab.Screen name="Rechercher" component={Search}/>
+      //     <Tab.Screen name="FilmDetail" options={{ title: 'Film detail' }} component={FilmDetail}/>
+      //   </Tab.Navigator>
+      // </NavigationContainer>
+    );
+  }
+}
+
+
+export default Navigation
